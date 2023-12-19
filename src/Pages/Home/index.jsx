34b18c6b8306e1980315704691454c7219d324c6
Samuel Sanabria <samuel.sanpo@hotmail.com>
2023-12-18 22:31:43 -0500

    fetch products data

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,28 @@
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
 function Home() {
 
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, []);
+
   return (
     <>
       <Layout>
         Home
-        <Card/>
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+          {
+            items?.map((item) => {
+              return <Card key={item.id} data={item} />
+            })
+          }
+        </div>
       </Layout>
     </>
   )
